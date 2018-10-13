cbe984b0db268c760c59bebbd02399086b3cffa7
J4YRY4N <43991557+J4YRY4N@users.noreply.github.com>
2018-10-13 18:30:07 +1100

    Create index.js
    Just trying to work out how to build a new Homebridge plugin for a Medion Life+ Smart Plug

---
+++ index.js
@@ -0,0 +1,73 @@
+const Service, Characteristic;
+ 
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("switch-plugin", "MedionLifePlusSmartPlug", mySwitch);
+};
+
+mySwitch.prototype = {
+  getServices: function () {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "Medion")
+      .setCharacteristic(Characteristic.Model, "S85225")
+      .setCharacteristic(Characteristic.SerialNumber, "000272/18");
+ 
+    let switchService = new Service.Switch("Medion Switch One");
+    switchService
+      .getCharacteristic(Characteristic.On)
+        .on('get', this.getSwitchOnCharacteristic.bind(this))
+        .on('set', this.setSwitchOnCharacteristic.bind(this));
+ 
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+  }
+};
+
+const request = require('request');
+const url = require('url');
+ 
+function mySwitch(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+ 
+mySwitch.prototype = {
+ 
+  getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+        url: me.getUrl,
+        method: 'GET',
+    }, 
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next(null, body.currentState);
+    });
+  },
+   
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'targetState': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
